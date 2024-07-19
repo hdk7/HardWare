cfb571612f347908a7908a7b3232366dfa81a9ac
hdk7 <142775539+hdk7@users.noreply.github.com>
2024-07-19 15:58:11 +0530

    Update App.jsx

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -12,7 +12,7 @@ import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
 
-  const url = "http://localhost:4000"
+  const url = "https://hardware-m580.onrender.com"
   return (
     <div>
       <ToastContainer></ToastContainer>
